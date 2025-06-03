b76ff4d7cda25cf9ddab3eac20116c76a7843f07
Gabriel Martins <gabrielmartins23445@gmail.com>
2025-06-03 17:04:36 -0300

    alterações iniciais

--- routes/consultas.js
+++ routes/consultas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const consultaController = require('../controllers/consultaController');
+const consultaController = require('../controllers/consultaControllers');
 
 router.post('/', consultaController.criarConsulta);
 // você pode adicionar rota GET aqui também
