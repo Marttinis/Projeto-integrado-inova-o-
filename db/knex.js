e97e416ba91e32191da1b596ba1eee016250e759
Gabriel Martins <gabrielmartins23445@gmail.com>
2025-06-03 17:57:25 -0300

    finalização da api

--- db/knex.js
+++ db/knex.js
@@ -3,7 +3,7 @@ const knex = require('knex')({
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : 'gabmed123',
+    password : 'Gabmed123',
     database : 'consultas_db'
   }
 });
